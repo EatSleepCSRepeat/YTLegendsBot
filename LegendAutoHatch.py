328323beca6f255852109935206812cba163aacb
Eat, Sleep, C#, Repeat <163324856+EatSleepCSRepeat@users.noreply.github.com>
2024-08-30 15:45:32 -0400

    Add files via upload

---
+++ LegendAutoHatch.py
@@ -0,0 +1,11 @@
+import pyautogui
+pyautogui.FAILSAFE = False
+while True:
+    pyautogui.press("R")
+    pyautogui.press("E")
+    pyautogui.press("E")
+    pyautogui.press("E")
+    pyautogui.press("E")
+    pyautogui.press("E")
+    pyautogui.press("E")
+    pyautogui.press("E")
